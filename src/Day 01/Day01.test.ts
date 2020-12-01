b9e961aca15b8cb046cd6bd5aa2174ae289298fa
Cody Swartz <ctsstc@gmail.com>
2020-12-01 01:03:01 -0800

    Finish: Day 1 with tests

--- src/Day 01/Day01.test.ts
+++ src/Day 01/Day01.test.ts
@@ -1,4 +1,4 @@
-import Day01 from './Day01';
+import Day01, { answer } from './Day01';
 
 describe('Day01', () => {
   const expenseReport = [1721, 979, 366, 299, 675, 1456];
@@ -7,5 +7,14 @@ describe('Day01', () => {
   it('finds two items that sum to 2020 and returns their product', () => {
     const answer = Day01(expenseReport);
     expect(answer).toEqual(product)
+  });
+
+  it('has the right answer', () => {
+    expect(answer).toEqual(437931);
+  });
+
+  it('has no answer', () => {
+    const answer = Day01([1, 2, 3]);
+    expect(answer).toEqual(-1);
   });
 });
--- src/Day 01/Day01.ts
+++ src/Day 01/Day01.ts
@@ -1,5 +1,24 @@
-export default function Day01(expenseReport: Array<number>): number {
-  expenseReport.sort()
+import { readFileSync } from 'fs';
+import { join as pathJoin } from 'path';
 
-  return 2;
+const path = pathJoin(__dirname, 'input.txt');
+const lines = readFileSync(path)
+  .toString()
+  .split("\n")
+  .map(str => parseInt(str));
+
+export default function Day01(expenseReport: Array<number>): number {
+  expenseReport.sort();
+  for(let i = 0; i < expenseReport.length - 1; i++) {
+    for(let ii = i + 1; ii < expenseReport.length; ii++) {
+      const first = expenseReport[i];
+      const second = expenseReport[ii];
+      if (first + second == 2020) {
+        return first * second;
+      }
+    }
+  }
+  return -1;
 }
+
+export const answer = Day01(lines);
