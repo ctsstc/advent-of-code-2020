ee0b8567603d85ad59ba94d1253985c19e68495e
Cody Swartz <ctsstc@gmail.com>
2020-12-02 21:00:27 -0800

    Add: Day 00 Templates

---
+++ src/Day 00/Day00.test.ts
@@ -0,0 +1,7 @@
+import { lines } from './Day00';
+
+describe('Day00', () => {
+  it('reads the file', () => {
+    expect(lines).toEqual(['Hello', 'World']);
+  });
+});
---
+++ src/Day 00/Day00.ts
@@ -0,0 +1,7 @@
+import { readFileSync } from 'fs';
+import { join as pathJoin } from 'path';
+
+const path = pathJoin(__dirname, 'input.txt');
+export const lines = readFileSync(path)
+  .toString()
+  .split("\n");
