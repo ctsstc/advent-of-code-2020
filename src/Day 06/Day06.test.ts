43ac2b2f33bf27f2210f1e675baab78751b21328
Cody Swartz <ctsstc@gmail.com>
2020-12-05 23:21:05 -0800

    Day 06: refactor out old code and tests, class'ified

--- src/Day 06/Day06.test.ts
+++ src/Day 06/Day06.test.ts
@@ -1,70 +1,50 @@
-import { Counter, Grouper, exampleLines, lines, Hasher, CountHasher, groups, singleLineGroups, hashes, round1, allVote, round2 } from './Day06';
+import { Answers, lines, exampleLines } from './Day06';
 
 describe('Day06', () => {
   describe('Example Tests', () => {
-    const groups = Grouper(exampleLines);
-    const hasher = Hasher(groups);
+    const answers = new Answers(exampleLines);
 
-    it('reads the file', () => {
-      expect(groups).toEqual([
-        ['abc'],
-        ['a', 'b', 'c'],
-        ['ab', 'ac'],
-        ['a', 'a', 'a', 'a'],
-        ['b']
-      ]);
-    });
-
-    it('Counts ', () => {
-      expect(Counter(groups)).toEqual(11);
-    });
-
-    it('hashes', () => {
-      expect(hasher).toEqual([{"a": 1, "b": 1, "c": 1}, {"a": 1, "b": 1, "c": 1}, {"a": 2, "b": 1, "c": 1}, {"a": 4}, {"b": 1}]);
-    });
-
-    it('counts round 2', () => {
-      expect(CountHasher(hasher, groups)).toEqual(6);
-    });
-  });
-
-  describe('Real Meal Deal', () => {
-    const groups = Grouper(lines);
-    const hasher = Hasher(groups);
-
-    it('Counts', () => {
-      expect(Counter(groups)).toEqual(6161);
-    });
-
-    it('counts round 2', () => {
-      expect(CountHasher(hasher, groups)).toEqual(2971);
-    });
-  });
-
-  describe('testing', () => {
-
-    it('groups', () => {
-      expect(groups).toHaveLength(5);
+    it('groups properly', () => {
+      expect(answers.groups).toHaveLength(5);
     });
 
     it('removes spaces in groups', () => {
-      expect(singleLineGroups).toEqual(["abc", "abc", "abac", "aaaa", "b"]);
+      expect(answers.singleLineGroups).toEqual(["abc", "abc", "abac", "aaaa", "b"]);
     });
 
     it('hashes groups', () => {
-      expect(hashes).toEqual([{"a": 1, "b": 1, "c": 1}, {"a": 1, "b": 1, "c": 1}, {"a": 2, "b": 1, "c": 1}, {"a": 4}, {"b": 1}]);
-    });
-
-    it('round1', () => {
-      expect(round1).toEqual(11);
+      expect(answers.hashes).toEqual([
+        {"a": 1, "b": 1, "c": 1},
+        {"a": 1, "b": 1, "c": 1},
+        {"a": 2, "b": 1, "c": 1},
+        {"a": 4},
+        {"b": 1}]
+      );
     });
 
     it('allVote', () => {
-      expect(allVote).toEqual([["a", "b", "c"], [], ["a"], ["a"], ["b"]]);
+      expect(answers.allVote).toEqual([["a", "b", "c"], [], ["a"], ["a"], ["b"]]);
+    });
+
+    it('round1', () => {
+      expect(answers.round1).toEqual(11);
     });
 
     it('round2', () => {
-      expect(round2).toEqual(6);
+      expect(answers.round2).toEqual(6);
+    });
+  });
+
+  describe('Real Tests', () => {
+    const answers = new Answers(lines);
+
+    it('round1', () => {
+      expect(answers.round1).toEqual(6161);
+    });
+
+    it('round2', () => {
+      expect(answers.round2).toEqual(2971);
+      
     });
   });
 });
--- src/Day 06/Day06.ts
+++ src/Day 06/Day06.ts
@@ -2,90 +2,27 @@ import { readFileSync } from 'fs';
 import { join as pathJoin } from 'path';
 
 const path = pathJoin(__dirname, 'input.txt');
-export const lines = readFileSync(path)
-  .toString()
-  .split("\n");
+const examplePath = pathJoin(__dirname, 'example.txt');
+export const lines = readFileSync(path).toString();
+export const exampleLines = readFileSync(examplePath).toString();
 
-export const exampleLines = readFileSync(pathJoin(__dirname, 'example.txt'))
-  .toString()
-  .split("\n");
+export class Answers {
+  constructor(private lines: string) {}
 
+  get groups() { return this.lines.split("\n\n"); }
+  get singleLineGroups() { return this.groups.map((group) => group.replace(/\s/g, '')); }
+  get groupVoterCount() { return this.groups.map((group) => group.split("\n").length); }
+  get hashes() { return this.singleLineGroups.map((group)=> group.split('').reduce(this.hasher, {}));}
 
-export const testingLines = readFileSync(pathJoin(__dirname, 'example.txt')).toString();
+  get allVote() { return this.hashes.map((hash, idx) => Object.keys(hash)
+    .filter((character) => hash[character] == this.groupVoterCount[idx])); }
 
-export const groups = testingLines.split("\n\n");
-export const singleLineGroups = groups.map((group) => group.replace(/\s/g, ''));
-export const hashes = singleLineGroups.map((group)=> {
-  return group.split('').reduce((hash, character) => {
+  get round1() { return this.hashes.reduce((count: number, hash) => count + Object.keys(hash).length, 0); }
+  get round2() { return this.allVote.reduce((count: number, group: string[]) => count + group.length, 0); }
+  
+  private hasher(hash, character) {
     const val = hash[character];
     hash[character] = val ? val + 1 : 1;
     return hash;
-  }, {});
-});
-
-export const round1 = hashes.reduce((count: number, hash) => count + Object.keys(hash).length, 0);
-
-export const groupVoterCount = groups.map((group) => group.split("\n").length);
-export const allVote = hashes.map((hash, idx) => Object.keys(hash)
-  .filter((character) => hash[character] == groupVoterCount[idx]));
-
-export const round2 = allVote.reduce((count: number, group: string[]) => count + group.length, 0);
-
-export function Grouper(lines: Array<string>): Array<Array<string>> {
-
-  let group = [];
-  return lines.reduce((groups, line, idx) => {
-
-    if (line != '') {
-      group.push(line);
-    }
-
-    if (line == '' || idx == lines.length - 1) {
-      groups.push(group);
-      group = [];
-    }
-
-    return groups;
-  }, []);
-}
-
-function Unique(value, index, self) {
-  return self.indexOf(value) === index;
-}
-
-export function Counter(groups: Array<Array<string>>): number {
-  return groups.reduce((count, group) => {
-    const chars = group.join('');
-    const uniques = chars.split('').filter(Unique);
-    return count + uniques.length;
-  }, 0);
-}
-
-export function Hasher(groups: Array<Array<string>>) {
-  return groups.map((lines) => {
-    return lines.reduce((hash, line) => {
-      const chars = line.split('');
-      // console.log({chars, hash, line});
-
-      chars.forEach((char) => {
-        if (char in hash) {
-          hash[char]++;
-        }
-        else {
-          hash[char] = 1;
-        }
-      });
-
-      return hash;
-    }, {});
-  });
-}
-
-export function CountHasher(hasher, grouper) {
-  return hasher.reduce((count, hash, idx) => {
-    const groupLineCount = grouper[idx].length;
-    const matching = Object.values(hash).filter(value => value == groupLineCount);
-
-    return count + matching.length;
-  }, 0);
+  }
 }
