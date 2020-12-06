70467febef39fed81b512bec0adf0cb4dc424e87
Cody Swartz <ctsstc@gmail.com>
2020-12-06 00:19:51 -0800

    Day 06: move IGroup to its own file

--- src/Day 06/Day06.ts
+++ src/Day 06/Day06.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from 'fs';
 import { join as pathJoin } from 'path';
+import { IGroup } from './IGroup';
 
 const path = pathJoin(__dirname, 'input.txt');
 const examplePath = pathJoin(__dirname, 'example.txt');
@@ -13,18 +14,8 @@ export class Answers {
     this.groups = lines.split("\n\n").map(Group);
   }
 
-  get round1() { return this.groups.reduce((count, group) => count + group.votedOn.length, 0); }
-  get round2() { return this.groups.reduce((count, group) => count + group.allVoted.length, 0); }
-}
-
-interface IGroup {
-  lines: string[];
-  noSpacesStr: string;
-  characters: string[];
-  voterCount: number;
-  hash: object;
-  allVoted: string[];
-  votedOn: string[];
+  get round1() { return this.groups.reduce((sum, group) => sum + group.votedOn.length, 0); }
+  get round2() { return this.groups.reduce((sum, group) => sum + group.allVoted.length, 0); }
 }
 
 function Group(str): IGroup {
---
+++ src/Day 06/IGroup.ts
@@ -0,0 +1,9 @@
+export interface IGroup {
+  lines: string[];
+  noSpacesStr: string;
+  characters: string[];
+  voterCount: number;
+  hash: object;
+  allVoted: string[];
+  votedOn: string[];
+}
