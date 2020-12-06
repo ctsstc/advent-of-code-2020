3b758f333bb563b03956a4c2ef9c711217eae266
Cody Swartz <ctsstc@gmail.com>
2020-12-06 00:00:44 -0800

    Day 06: refactor allVote into Group

--- src/Day 06/Day06.ts
+++ src/Day 06/Day06.ts
@@ -7,7 +7,7 @@ export const lines = readFileSync(path).toString();
 export const exampleLines = readFileSync(examplePath).toString();
 
 export class Answers {
-  private groups: IGroup[];
+  public groups: IGroup[];
 
   constructor(lines: string) {
     this.groups = lines.split("\n\n").map(Group);
@@ -15,9 +15,7 @@ export class Answers {
 
   get groupVoterCount() { return this.groups.map((group) => group.voterCount); }
   get hashes() { return this.groups.map((group)=> group.hash);}
-
-  get allVote() { return this.hashes.map((hash, idx) => Object.keys(hash)
-    .filter((character) => hash[character] == this.groupVoterCount[idx])); }
+  get allVote() { return this.groups.map(group => group.allVoted); }
 
   get round1() { return this.hashes.reduce((count: number, hash) => count + Object.keys(hash).length, 0); }
   get round2() { return this.allVote.reduce((count: number, group: string[]) => count + group.length, 0); }
@@ -29,19 +27,24 @@ interface IGroup {
   characters: string[];
   voterCount: number;
   hash: object;
+  allVoted: string[];
 }
 
 function Group(str): IGroup {
   const lines = str.split("\n");
   const noSpacesStr = str.replace(/\s/g, '');
   const characters = noSpacesStr.split('');
+  const voterCount = lines.length;
+  const hash = characters.reduce(hashReducer, {});
+  const allVoted = Object.keys(hash).filter((character) => hash[character] == voterCount);
 
   return {
     lines,
     noSpacesStr,
     characters,
-    voterCount: lines.length,
-    hash: characters.reduce(hashReducer, {})
+    voterCount,
+    hash,
+    allVoted,
   }
 }
 
