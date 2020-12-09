0ea7a79abf6c8f27ab572a99875f3b076c8331c2
Cody Swartz <ctsstc@gmail.com>
2020-12-08 21:22:02 -0800

    Day 00: Update template

--- src/Day 00/Day00.test.ts
+++ src/Day 00/Day00.test.ts
@@ -1,7 +1,22 @@
-import { lines } from './Day00';
+
+import { readFileSync } from 'fs';
+import { join as pathJoin } from 'path';
+
+const path = pathJoin(__dirname, 'input.txt');
+const lines = readFileSync(path)
+  .toString().trim()
+  .split("\n");
+
+const examplePath = pathJoin(__dirname, 'example.txt');
+const exampleLines = readFileSync(examplePath)
+  .toString().trim()
+  .split("\n");
 
 describe('Day00', () => {
   it('reads the file', () => {
     expect(lines).toEqual(['Hello', 'World']);
+  });
+  it('reads the example', () => {
+    expect(exampleLines).toEqual(['Example']);
   });
 });
--- src/Day 00/Day00.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from 'fs';
-import { join as pathJoin } from 'path';
-
-const path = pathJoin(__dirname, 'input.txt');
-export const lines = readFileSync(path)
-  .toString()
-  .split("\n");
