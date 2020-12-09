29a8ad8397c97cfd2149660c36e61b917d54ae7c
Cody Swartz <ctsstc@gmail.com>
2020-12-08 22:42:12 -0800

    Day 09: solve part 2 with tests

--- src/Day 09/Day09.test.ts
+++ src/Day 09/Day09.test.ts
@@ -1,7 +1,7 @@
 
 import { readFileSync } from 'fs';
 import { join as pathJoin } from 'path';
-import { Solver } from './Day09';
+import { Part2, Solver } from './Day09';
 
 const path = pathJoin(__dirname, 'input.txt');
 const lines = readFileSync(path)
@@ -14,10 +14,26 @@ const exampleLines = readFileSync(examplePath)
   .split("\n");
 
 describe('Day09', () => {
-  it('reads the file', () => {
-    expect(Solver(lines, 25)).toEqual(1124361034);
+  const badExampleNumber = Solver(exampleLines, 5);
+  const badNumber = Solver(lines, 25);
+
+  describe('Part 1', () => {
+    it('solves the example', () => {
+      expect(badExampleNumber).toEqual(127);
+    });
+
+    it('solves the real deal', () => {
+      expect(badNumber).toEqual(1124361034);
+    });
   });
-  it('reads the example', () => {
-    expect(Solver(exampleLines, 5)).toEqual(127);
+
+  describe('Part 2', () => {
+    it('solves the example', () => {
+      expect(Part2(exampleLines, badExampleNumber)).toEqual(62)
+    });
+
+    it('solves the real meal deal', () => {
+      expect(Part2(lines, badNumber)).toEqual(129444555);
+    });
   });
 });
--- src/Day 09/Day09.ts
+++ src/Day 09/Day09.ts
@@ -18,7 +18,7 @@ export function Solver(linesStr: string[], preambleLength: number): number {
     // https://stackoverflow.com/questions/23305747/javascript-permutation-generator-with-permutation-length-parameter
     // not what we need though, we need uniqueness
     for(let ii = 0; ii < frame.length; ii++) {
-      const currentLineIdx = i + ii;
+      // const currentLineIdx = i + ii;
       // if (usedLines.includes(currentLineIdx)) { continue; }
 
       const frameCurrent = frame[ii];
@@ -38,4 +38,50 @@ export function Solver(linesStr: string[], preambleLength: number): number {
   }
 
   return -1;
+}
+
+export function Part2(linesStr: string[], badNumber: number): number {
+  const lines = linesStr.map(line => parseInt(line));
+  const [first, second] = lines.slice(0, 2);
+  const badNumberIdx = lines.indexOf(badNumber);
+  let runningTotal = first + second;
+  let startIdx = 0;
+  let endIdx = 1;
+
+  // wishful hopes
+  if (runningTotal == badNumber) {
+    return runningTotal;
+  }
+
+  while (startIdx < badNumberIdx) {
+
+    // console.log({startIdx, endIdx, runningTotal});
+
+    if (runningTotal < badNumber) {
+      const next = lines[++endIdx];
+      runningTotal += next;
+    }
+
+    if (runningTotal == badNumber) {
+      break;
+    }
+
+    if (runningTotal > badNumber) {
+      const front = lines[startIdx];
+      runningTotal -= front;
+      startIdx++;
+    }
+
+    if (runningTotal == badNumber) {
+      break;
+    }
+  }
+
+  const range = lines.slice(startIdx, endIdx + 1);
+  const smol = Math.min(...range);
+  const beeg = Math.max(...range);
+
+  // console.log({badNumber, startIdx, endIdx, range, smol, beeg});
+
+  return smol + beeg;
 }
