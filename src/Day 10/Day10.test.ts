0330a6e701bc65c514e661406173daa8650ac931
Cody Swartz <ctsstc@gmail.com>
2020-12-09 23:42:09 -0800

    Day 10: Solve with memoization

--- src/Day 10/Day10.test.ts
+++ src/Day 10/Day10.test.ts
@@ -19,17 +19,31 @@ const exampleLines2 = readFileSync(examplePath2)
   .split("\n");
 
 describe('Day10', () => {
-  it('Example 1', () => {
-    const sol = new Day10(exampleLines).solution()
-    console.log({sol});
-    expect(sol['1'] * sol['3']).toEqual(7*5);
+  describe('Solution 1', () => {
+    it('Example 1', () => {
+      const sol = new Day10(exampleLines).solution()
+      console.log({sol});
+      expect(sol['1'] * sol['3']).toEqual(7*5);
+    });
+    it('Example 2', () => {
+      const sol = new Day10(exampleLines2).solution();
+      expect(sol['1'] * sol['3']).toEqual(22*10);
+    });
+    it('Real Meal Deal', () => {
+      const sol = new Day10(lines).solution();
+      expect(sol['1'] * sol['3']).toEqual(2240);
+    });
   });
-  it('Example 2', () => {
-    const sol = new Day10(exampleLines2).solution();
-    expect(sol['1'] * sol['3']).toEqual(22*10);
-  });
-  it('Real Meal Deal', () => {
-    const sol = new Day10(lines).solution();
-    expect(sol['1'] * sol['3']).toEqual(2240);
+
+  describe('Solution 2', () => {
+    it('Example 1', () => {
+      expect(new Day10(exampleLines).solution2()).toEqual(8);
+    });
+    it('Example 2', () => {
+      expect(new Day10(exampleLines2).solution2()).toEqual(19208);
+    });
+    it('Real Meal Deal', () => {
+      expect(new Day10(lines).solution2()).toEqual(99214346656768);
+    });
   });
 });
--- src/Day 10/Day10.ts
+++ src/Day 10/Day10.ts
@@ -1,19 +1,17 @@
 
+const numberSort = (a, b) => a - b;
+
 export class Day10 {
   private lines: number[] = this.linesStr.map(line => parseInt(line));
   constructor(private linesStr: string[]) { }
 
   solution(): {} {
     this.lines.push(0);
-    this.lines.sort((a, b) => a - b);
-
-    // console.log(this.lines);
+    this.lines.sort(numberSort);
 
     const collection = { '3': 1 }; // the last connection
     for (let i = 0; i < this.lines.length - 1; i++) {
       const diff = this.lines[i + 1] - this.lines[i];
-
-      // console.log(this.lines[i + 1], this.lines[i], {diff});
 
       if (diff in collection) {
         collection[diff]++;
@@ -23,8 +21,45 @@ export class Day10 {
       }
     }
 
-
-
     return collection;
   }
+
+  solution2(): number {
+    this.lines.push(0);
+    this.lines.sort(numberSort);
+
+    const memo:Map<number, number> = new Map();
+    memo.set(0, 1); // the device and first adapter
+
+    for (let i = 1; i < this.lines.length; i++) {
+      const possibilities = this.plugabilities(i);
+
+      let sum = 0;
+      for(let ii = 1; ii <= possibilities; ii++) {
+        sum += memo.get(i - ii)
+      }
+
+      memo.set(i, sum);
+    }
+
+    return memo.get(this.lines.length - 1);
+  }
+
+  private plugabilities(idx: number) {
+    const current = this.lines[idx];
+    let count = 0;
+
+    if (idx > 0 && current - this.lines[idx-1] <= 3) {
+      count++;
+    }
+    if (idx > 1 && current - this.lines[idx-2] <= 3) {
+      count++;
+    }
+    if (idx > 2 && current - this.lines[idx-3] <= 3) {
+      count++;
+    }
+
+    return count;
+  }
+
 }
