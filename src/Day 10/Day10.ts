69bd0fd994051a109830deb0d694c2ff48b0b2fd
Cody Swartz <ctsstc@gmail.com>
2020-12-10 00:55:06 -0800

    Day 10: refactor, clean up

--- src/Day 10/Day10.ts
+++ src/Day 10/Day10.ts
@@ -3,33 +3,25 @@ const numberSort = (a, b) => a - b;
 
 export class Day10 {
   private lines: number[] = this.linesStr.map(line => parseInt(line));
-  constructor(private linesStr: string[]) { }
-
-  solution(): {} {
+  constructor(private linesStr: string[]) {
     this.lines.push(0);
     this.lines.sort(numberSort);
+  }
 
-    const collection = { '3': 1 }; // the last connection
+  solution(): {} {
+    const differencesCount = { 1: 0, 2: 0, 3: 1 }; // 3 for the last one
+
     for (let i = 0; i < this.lines.length - 1; i++) {
       const diff = this.lines[i + 1] - this.lines[i];
-
-      if (diff in collection) {
-        collection[diff]++;
-      }
-      else {
-        collection[diff] = 1;
-      }
+      differencesCount[diff]++;
     }
 
-    return collection;
+    return differencesCount;
   }
 
   solution2(): number {
-    this.lines.push(0);
-    this.lines.sort(numberSort);
-
     const memo:Map<number, number> = new Map();
-    memo.set(0, 1); // the device and first adapter
+    memo.set(0, 1); // the device and first adapter; 1 possibility
 
     for (let i = 1; i < this.lines.length; i++) {
       const possibilities = this.plugabilities(i);
@@ -51,7 +43,7 @@ export class Day10 {
 
     for (let i = 0; i < amount; i++) {
       const offset = i + 1;
-      if (idx > i && current - this.lines[idx - offset] <= 3) {
+      if (idx > i && current - this.lines[idx - offset] <= amount) {
         count++;
       }
     }
