559e3e5cae7002845a7b4cba3ede0df97a11212b
Cody Swartz <ctsstc@gmail.com>
2020-12-19 21:01:22 -0800

    Day 20: start

---
+++ src/Day 20/Day20.test.ts
@@ -0,0 +1,27 @@
+
+import { readFileSync } from 'fs';
+import { join as pathJoin } from 'path';
+
+// const path = pathJoin(__dirname, 'input.txt');
+// const lines = readFileSync(path)
+//   .toString().trim()
+//   .split("\n");
+
+const examplePath = pathJoin(__dirname, 'example.txt');
+const exampleLines = readFileSync(examplePath)
+  .toString().trim()
+  .split("\n");
+
+describe('Day20', () => {
+  describe('Example Data', () => {
+    it('reads the example', () => {
+      expect(exampleLines).toEqual(['Example']);
+    });
+  });
+
+  // describe('Real Data', () => {
+  //   it('reads the file', () => {
+  //     expect(lines).toEqual(['Hello', 'World']);
+  //   });
+  // });
+});
